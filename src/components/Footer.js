8b579a1c96b7c5c2bb18a3f2b8da9d4e5126920e
Nick Garner <nickgcs13@gmail.com>
2023-06-07 23:46:51 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,21 +10,18 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6}>
-            <h3>Nicholas</h3>
+            <h3>Nicholas Garner</h3>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/nicholasgarner13/">
                 <img src={navIcon1} alt="Icon" className="icon" />
               </a>
-              <a href="#">
-                <img src={navIcon2} alt="Icon" className="icon" />
-              </a>
               <a href="https://github.com/kegkeg10">
                 <img src={navIcon3} alt="Icon" className="icon" />
               </a>
             </div>
-            <p>Copyright Nicholas Garner 2023. All Rights Reserved</p>
+            <p className="allright">Copyright Nicholas Garner 2023. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
